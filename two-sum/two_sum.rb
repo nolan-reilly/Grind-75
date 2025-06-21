5b87935ec11c725f43cc1d053a75b0b8293659fc
Nolan Reilly <nolanreilly02@gmail.com>
2025-06-21 08:28:41 -0500

    Adopt new commenting style

--- two-sum/two_sum.rb
+++ two-sum/two_sum.rb
@@ -1,29 +1,39 @@
-# Things learned from this problem
-# - #each_with_index (Basically just an each loop but you get access to its current index)
-# - Ruby isn't like python where you can just say in for Arrays or Hashes
-# - Instead of in we can use #include?, #key?, #value? for arrays or hashes
+# -----------------------------------------------------------------------------
+# Things I Learned From This Problem
+# -----------------------------------------------------------------------------
+# - '.each_with_index': Like .each, but also gives access to the index
+# - In Ruby, you can't use Python style of 'in' checks for array or hashes
+# - Instead we use:
+#   - '.include?' -> for arrays
+#   - '.key?' or '.value?' -> for hashes
+# -----------------------------------------------------------------------------
 
 
 def two_sum(nums, target)
-    # O(n) Solution: Storing every value we search in a hash
-    # ---------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # O(n) Solution
+    # Idea: Use a hash map to store previously seen numbers. For each number,
+    # check if (target - num) exists in the hash.
+    # -------------------------------------------------------------------------
 
     hash = {}
 
     nums.each_with_index do |num, i|
-        # Check if the value is already looped through
-        val = target - num
-        if hash.key?(val)
-            return [i, hash[val]]
+        complement = target - num
+
+        # If the complement is found, return its idx and the current idx
+        if hash.key?(complement)
+            return [i, hash[complement]]
         end
 
-        # If we didn't find a match store the number
-        hash[num] = i
+        hash[num] = i # If we didn't find a match store the number
     end
 
-
-    # Brute Force Solution O(n^2): Check every possible combination
-    # ---------------------------------------------------------------------------
+    
+    # -------------------------------------------------------------------------
+    # Brute Force O(n^2)
+    # Idea: Check all possible pairs
+    # -------------------------------------------------------------------------
 
     nums.each_with_index do |x, i|
         nums.each_with_index do |y, j|
